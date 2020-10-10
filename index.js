ba1e9c365700c831860c8a0fd7e7fecd622478cb
Naufaldi Athallah Rifqi <naufaldi.rifqi@akseleran.com>
2020-10-10 09:12:41 +0700

    endpoint ticket

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require("axios");
+var parser = require('xml2json-light');
 
 // Constants
 const PORT = 8000;
@@ -19,6 +21,21 @@ app.get('/', (req, res) => {
         'message': "Hello World!"
     })
 })
+
+app.get("/user/:ticketId", async (req, res, next) => {
+    const ticket = req.params.ticketId
+    const URL_REDIRECT = "http://localhost:8000"
+    try {
+        const data = await axios.post(
+            `https://sso.ui.ac.id/cas2/serviceValidate?ticket=${ticket}&service=${URL_REDIRECT}`
+        )
+        var json = parser.xml2json(data.data)
+        console.log(json)
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 app.listen(PORT);
 console.log('Server listening on port: ' + PORT);
